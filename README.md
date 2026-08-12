03a36599c82a406e87a71452ac31844c7b456d72
Horia Bosoanca <redplayer333hhh@gmail.com>
2026-08-12 19:40:31 +0300

    README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+## Required settings
+### For the overlay to work:
+- in-game settings->Video->General->Window Mode->Borderless
+### To make the overlay transparent instead of black, set either:
+1) Settings->System->Display->Graphics->Add Desktop App (browse the app)->GPU preference->set to a non-nvidia gpu
+2) Nvidia Control Panel->Manage 3D settings->OpenGL GDI compatibility->Prefer compatible
--- main/main.go
+++ main/main.go
@@ -6,13 +6,6 @@ import (
 	"fmt"
 )
 
-/*
-!!! window will not be transparent unless you set either:
-1) Settings->System->Display->Graphics->Add Desktop App (browse the app)
-->GPU preference->set to intel integrated graphics, not nvidia
-2) Nvidia Control Panel->Manage 3D settings
-->OpenGL GDI compatibility->Prefer compatible
-*/
 func main() {
 	clientData, err := data.Give_data("https://127.0.0.1:2999")
 	if err != nil {
